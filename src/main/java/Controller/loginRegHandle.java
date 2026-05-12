e91152e084c83c53e18bd681f2427fb9b555f159
Toriqul2023 <toriqulislam296@yahoo.com>
2026-05-12 23:32:15 +0600

    methodoverloading

--- src/main/java/Controller/loginRegHandle.java
+++ src/main/java/Controller/loginRegHandle.java
@@ -4,6 +4,7 @@ import Model.User;
 import com.google.gson.Gson;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
@@ -20,12 +21,30 @@ public class loginRegHandle {
             file.write(gson.toJson(newUsers));
             file.close();
 
-
-
         }
         catch (IOException e) {
             System.out.println(e);
         }
        return user;
+    }
+    public void handleLogIn(int id,String password) throws IOException{
+        User[] users=gson.fromJson(
+                new FileReader("src/main/resources/user.json"),User[].class);
+        for(User user : users){
+            if(user.getId()==id && user.getPassword().equals(password)){
+                System.out.println("Log in successful");
+                // Proper Working is still needed it is just a sample of method overloading.
+            }
+        }
+    }
+    public void handleLogIn(String email,String password) throws IOException {
+        User[] users=gson.fromJson(
+                new FileReader("src/main/resources/user.json"),User[].class);
+        for(User user : users){
+            if(user.getEmail().equals(email) && user.getPassword().equals(password)){
+                System.out.println("Log in successful");
+                // Proper Working is still needed it is just a sample of method overloading.
+            }
+        }
     }
 }
